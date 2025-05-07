c6e0c38e796034d7dbbfca8ff449b49adc67ee5f
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-07 21:09:36 +0300

    Add new project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [require('path').join(__dirname, 'src/styles')],
+  },
 };
 
 export default nextConfig;
