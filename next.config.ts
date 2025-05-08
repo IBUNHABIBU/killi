48e732f58d92bffd83f32be6e94658e8c8e0b82b
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-08 11:34:37 +0300

    add files

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [require('path').join(__dirname, 'src/styles')],
-    prependData: `@import "variables";`, // Global Sass vars
-  },
 };
 
 export default nextConfig;
