7217d87d191b789a6891af06ed19654102ddcb00
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-14 10:43:34 +0300

    Add about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,11 @@ import { SpecialOffers } from "./components/SpecialOffers/SpecialOffers";
 export default function Home() {
   return (
     <div className={styles.page}>
-     <Head>
+      <Head>
         <title>Adventure Tourism | Explore the World</title>
         <meta name="description" content="Discover amazing adventures with our tourism services" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navigation />
       <main>
       <Hero />
       <CampingSection />
