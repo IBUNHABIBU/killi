d60612f2ff2df007d80b877ee82c9f75f357ae7b
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-15 19:42:09 +0300

    Add smooth scroll

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -4,7 +4,6 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from './Hero.module.scss';
-import { useRouter } from 'next/navigation';
 import { useScrollToSection } from '../utils/ScrollToSection';
 
 export const Hero = () => {
