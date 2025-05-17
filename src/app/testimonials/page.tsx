f33e37caf7e5728e9c1216dc390d99a7b8a175a0
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-18 02:01:32 +0300

    Fix type error to return promise

---
+++ src/app/testimonials/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TestimonialSection } from '../components/TestimonialSection/TestimonialSection'
+
+const page = () => {
+  return (
+    <div>
+        <TestimonialSection />
+    </div>
+  )
+}
+
+export default page
