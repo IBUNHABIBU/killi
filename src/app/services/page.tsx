86a7d39992c133b7b18173527d29cdae30e5c165
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-19 05:11:52 +0300

    Add Icons

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import UpcomingTours from '../components/UpcomingTours/UpcomingTours'
+import Services from '../components/services/Services'
 
 const page = () => {
   return (
     <div>
-        <UpcomingTours />
+        <Services />
     </div>
   )
 }
