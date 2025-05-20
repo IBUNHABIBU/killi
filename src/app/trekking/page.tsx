f43a37ad17a0c375e8002bbc6ad5fae89e00c488
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-20 16:53:29 +0300

    Add Icons

---
+++ src/app/trekking/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UpcomingTours from '../components/UpcomingTours/UpcomingTours'
+
+const page = () => {
+  return (
+    <div>
+        <UpcomingTours />
+    </div>
+  )
+}
+
+export default page
