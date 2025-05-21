160d401e12445f5f2c463dc87da5f82be541dbe1
IBUNHABIBU <ibunhabib@gmail.com>
2025-05-21 14:50:24 +0300

    Add wildlife pages

---
+++ src/app/components/WildLifeSafari/WildLifeSafari.tsx
@@ -0,0 +1,21 @@
+import { wildlife } from '@/app/data/tour';
+import styles from '../UpcomingTours/UpcomingTours.module.scss';
+import TourCard from '../UpcomingTours/TourCard';
+
+const WildLifeSafari = () => {
+  return (
+    <section className={styles.section}>
+      <div className={styles.grid}>
+        {wildlife.map((tour) => (
+          <TourCard 
+            key={tour.id}
+            tour={tour}
+            className={tour.id === 'marangu-route' ? styles.featuredCard : ''}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default WildLifeSafari;
